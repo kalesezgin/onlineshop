3ce2b95197738b44a745f8d6f1830c9f715c04bc
Sezgin Kale <kalesezgin@gmail.com>
2023-04-18 17:39:39 +0300

    CategoryListModel, CategoryItem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './categories.styles.scss'
+import List from './components/category-list/category-list.component'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <List/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
